a70d5cf6564c14fe05b6e1c513ad294f69c2aa50
futuressh <3038242641@qq.com>
2024-01-20 22:29:39 +0800

    Updated install scripts

--- setup_env.ps1
+++ setup_env.ps1
@@ -1,15 +1,8 @@
-#!/bin/bash
-
-# Download and install Miniconda
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py310_23.9.0-0-MacOSX-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-
-# Initialize Conda
-source $HOME/miniconda/etc/profile.d/conda.sh
-conda init
+# Set execution policy for this session
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
 # Name of the conda environment
-ENV_NAME="pytorch_benchmark"
+$ENV_NAME="pytorch_benchmark"
 
 # Create a new conda environment with Python 3.10
 conda create --name $ENV_NAME python=3.10 -y
@@ -20,6 +13,7 @@ conda activate $ENV_NAME
 # Install PyTorch 2.1.1 with CUDA support
 conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=11.8 -c pytorch -c nvidia -y
 
+# Install pytorch-benchmark
 pip install pytorch-benchmark
 
 # Clone the repositories
